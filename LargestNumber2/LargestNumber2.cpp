c9154d632603c33ab786a27ccb82a27dea41da5a
kaooatprm <otekakong50@gmail.com>
2021-08-15 04:40:44 +0700

    Add project files.

---
+++ LargestNumber2/LargestNumber2.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main() {
+    double a, b, c;
+    printf("Enter three number: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if (a >= b && a >= c)
+        printf("%.2lf is the largest number.", a);
+    else if (b >= a && b >= c)
+        printf("%.2lf is the largest number.", b);
+    else
+        printf("%.2lf is the largest number.", c);
+    
+    return 0;
+}
